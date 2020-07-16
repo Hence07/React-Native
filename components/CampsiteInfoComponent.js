53199b8631b9b3d4039e30a2980294297cf55cdd
Prudence Codjia <pcodjia@Prudences-MBP.fios-router.home>
2020-07-16 18:28:41 -0400

    Forms

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -5,12 +5,19 @@ import { Card, Icon } from 'react-native-elements';
 import { COMMENTS } from '../shared/comments';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
-        comments: state.comments
+        comments: state.comments,
+        favorites: state.favorites
     };
+};
+
+const mapDispatchToProps = {
+    postFavorite: campsiteId => (postFavorite(campsiteId))
 };
 
 function RenderCampsite(props) {
@@ -64,21 +71,26 @@ function RenderComments({comments}) {
 
 class CampsiteInfo extends Component {
     
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            comments: COMMENTS,
-            favorite:false
-        };
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         campsites: CAMPSITES,
+    //         comments: COMMENTS,
+    //         favorite:false
+    //     };
+    // }
 
     static navigationOptions = {
         title: 'Campsite Information'
     };
-    markFavorite() {
-        this.setState({favorite: true});
+    // markFavorite() {
+    //     this.setState({favorite: true});
+    // }
+
+    markFavorite(campsiteId) {
+        this.props.postFavorite(campsiteId);
     }
+
     render() {
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.props.campsites.campsites.filter(campsite => campsite.id === campsiteId)[0];
@@ -86,8 +98,8 @@ class CampsiteInfo extends Component {
         return (
             <ScrollView>
                 <RenderCampsite campsite={campsite}
-                    favorite={this.state.favorite}
-                    markFavorite={() => this.markFavorite()}
+                    favorite={this.props.favorites.includes(campsiteId)}
+                    markFavorite={() => this.markFavorite(campsiteId)}
                 />
                 <RenderComments comments={comments} />
             </ScrollView>
@@ -95,4 +107,4 @@ class CampsiteInfo extends Component {
     }
 }
 
-export default connect(mapStateToProps)(CampsiteInfo);
+export default connect(mapStateToProps, mapDispatchToProps)(CampsiteInfo);
