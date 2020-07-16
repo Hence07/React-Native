3dc956c7198be32327c3fe78a77bfe0a22716bf3
Prudence Codjia <pcodjia@Prudences-MBP.fios-router.home>
2020-07-15 20:39:25 -0400

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,8 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
+
 
 const mapStateToProps = state => {
     return {
@@ -25,25 +27,42 @@ function Mission(){
 }
 
 class About extends Component{
-// constructor(props){
-//     super(props)
-//     this.state ={
-//         partners: PARTNERS
-//     }
-// }
+
     static navigationOptions = {
         title: 'About Us'
     };
 
-render(){
-    const renderPartner = ({item}) => {
-        return(
-        <ListItem
-        title={item.title}
-        subtitle={item.description}
-        leftAvatar={{source: {uri: baseUrl + item.image}}}
-        />
-        )
+    render() {
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                />
+            );
+        };
+    if (this.props.partners.isLoading) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Loading />
+                </Card>
+            </ScrollView>
+        );
+    }
+    if (this.props.partners.errMess) {
+        return (
+            <ScrollView>
+                <Mission />
+                <Card
+                    title='Community Partners'>
+                    <Text>{this.props.partners.errMess}</Text>
+                </Card>
+            </ScrollView>
+        );
     }
     return(
         <ScrollView>
